6409a7a02bb461a62f1742aeba2928931fe5fe50
Lucien <lygm1062@gmail.com>
2019-01-07 14:18:16 +0800

    修改节点地址

--- src/SicBo/SicBoRecords.js
+++ src/SicBo/SicBoRecords.js
@@ -15,7 +15,7 @@ import Eos from 'eosjs';
 const network = {
     blockchain: 'eos',
     protocol: 'https',
-    host: 'api-kylin.eosasia.one',  // filter-on = *
+    host: 'api-kylin.eoslaomao.com',  // filter-on = *
     port: '',
     chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191'  // kylin net
 }
